32ac4be14bebde95e648641e397f85f0a68487c5
zaki khan <mohammedkhanzaki@gmail.com>
2024-02-04 18:56:38 +0530

    SETUP

--- TEST.java
+++ TEST.java
@@ -1,3 +1,6 @@
-public static void main(String[] args) {
-    System.out.println("HELLO");
+class TEST{
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
 }
+
